98ed80d5e31f28bae5fa699f2d16622e3562f9e1
EFTEKHARZADEH Reza <Reza.EFTEKHARZADEH@apotek1.no>
2021-07-17 23:55:52 +0200

    Admin Component Created

---
+++ reactfrontend/frontend/src/component/ShopAdmin.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+
+class ShopAdmin extends Component{
+render(){
+    return(
+        <div>
+            <h1>here is Admin console</h1>
+        </div>
+    );
+ }
+}export default ShopAdmin;
