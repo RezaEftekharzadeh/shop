d03e2e3825ed7a5e2df42206954ea2f345eb212d
rezae <reza.eftekharzadeh@apotek1.no>
2022-05-01 00:34:56 +0200

    sm-122 creat connection to postgres

--- reactfrontend/frontend/src/component/ShopAdmin.jsx
+++ reactfrontend/frontend/src/component/ShopAdmin.jsx
@@ -1,28 +1,27 @@
 import React, {Component} from "react"
 import AdminPageService from "../service/AdminPageService";
 
-class ShopAdmin extends Component{
+class ShopAdmin extends Component {
     constructor(props) {
         super(props);
 
-        this.state={
-                admin: ""
+        this.state = {
+            admin: ""
         }
     }
+
     componentDidMount() {
-
         AdminPageService.getIndexPage().then(res => console.log(res));
+    }
 
-        }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.admin}</h1>
+                <h1>Hi</h1>
+            </div>
+        );
+    }
+}
 
-
-    render(){
-
-        return(
-        <div>
-            <h1>{this.state.admin}</h1>
-            <h1>Hi</h1>
-        </div>
-    );
- }
-}export default ShopAdmin;
+export default ShopAdmin;
